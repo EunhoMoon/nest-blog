797264c64d4fd7bccb4a8d3973c4f140ae32d541
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-05-22 17:45:31 +0900

    프로젝트 세팅

---
+++ src/blog.service.ts
@@ -0,0 +1,40 @@
+import { PostDto } from "./blog.model";
+
+export class BlogService {
+  posts = [];
+
+  getAllPosts() {
+    return this.posts;
+  }
+
+  createPost(postDto: PostDto) {
+    const id = this.posts.length + 1;
+    this.posts.push({
+      id: id.toString(),
+      ...postDto,
+      createAt: new Date()
+    });
+  }
+
+  getPost(id) {
+    const post = this.posts.find(post => {
+      return post.id === id;
+    });
+    console.log(post);
+    return post;
+  }
+
+  delete(id) {
+    const filteredPosts = this.posts.filter(post => post.id !== id);
+    this.posts = filteredPosts;
+  }
+
+  updatePost(id, postDto: PostDto) {
+    let updateIndex = this.posts.findIndex(post => post.id === id);
+    const updatePost = {id, ...postDto, updatedAt: new Date()}
+    this.posts[updateIndex] = updatePost;
+
+    return updatePost;
+  }
+}
+
